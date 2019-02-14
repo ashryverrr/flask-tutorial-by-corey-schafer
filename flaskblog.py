9332c23dd1f5069fa5ed8720758bb50f2096a4f2
BRYAN PATRICK C. CHUA <9chuab@ph.ibm.com>
2019-02-14 18:26:09 +0800

    Add sql alchemy and login dummy

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,33 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm 
+from datetime import datetime 
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    post = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}, {self.email}, {self.image_file} ')" 
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(user.id), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}, {self.date_posted} ')" 
+
 
 posts = [
     {
@@ -38,8 +65,14 @@ def register():
 def login():
    form = LoginForm()
    if form.validate_on_submit():
-       flash(f'Account Created for {form.username.data}!', 'success')
-       return redirect(url_for('home'))
+       
+       if form.email.data == "admin@blog.com" and form.password.data == "password":
+           flash(f'Login successfull for {form.email.data}!', 'success')
+           return redirect(url_for('home'))
+       else:
+           flash(f'Login failed. Please check email and password.', 'error')
+           return render_template('login.html', title='Login', form=form)
+       return render_template('login.html', title='Login', form=form)       
    else:    
        return render_template('login.html', title='Login', form=form)
 
